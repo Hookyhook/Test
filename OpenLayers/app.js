88616883d1b797a134ca931777f31222143e5852
Hookyhook <108137850+Hookyhook@users.noreply.github.com>
2023-01-13 20:35:07 +0100

    Removed interactions added working single marker

--- OpenLayers/app.js
+++ OpenLayers/app.js
@@ -1,38 +1,37 @@
 // create Stamen layer
 var stamenLayer = new ol.layer.Tile({
-    source: new ol.source.Stamen({
-        layer: 'toner'
-    })
+  source: new ol.source.Stamen({
+    layer: "toner",
+  }),
 });
 
 // create a map
-var map = new ol.Map({
-    target: 'map',
+  var map = new ol.Map({
+    target: "map",
     layers: [stamenLayer],
     view: new ol.View({
-        center: ol.proj.fromLonLat([48, 23]),
-        zoom: 5
-    })
-});
+      center: ol.proj.fromLonLat([0, 0]),
+      zoom: 1,
+    }),
+    controls: [],
+    interactions: [
+    ],
+  });
 
-
-map.getControls().forEach(function (control) {
-  control.setActive(false);
-});
 
 // code for adding marker when an address is typed in
 
 var markerFeature;
 var markerVectorLayer;
 var markerStyle = new ol.style.Style({
-    image: new ol.style.RegularShape({
-      fill: new ol.style.Fill({color: 'white'}),
-      stroke: new ol.style.Stroke({color: 'black', width: 1}),
-      points: 4,
-      radius: 8,
-      angle: Math.PI / 4
-    })
-  });
+  image: new ol.style.RegularShape({
+    fill: new ol.style.Fill({ color: "grey" }),
+    stroke: new ol.style.Stroke({ color: "black", width: 2 }),
+    points: 4,
+    radius: 8,
+    angle: Math.PI / 4,
+  }),
+});
 
 function searchAddress() {
   var address = document.getElementById("address").value;
@@ -62,12 +61,15 @@ function searchAddress() {
           source: vectorSource,
         });
         map.addLayer(markerVectorLayer);
-        map.getView().animate({ center: coord, zoom: 18 });
+        map.getView().animate({ center: coord, zoom: 16 });
       } else {
-        alert("No results found");
+        console.log("No results found");
       }
     })
     .catch(function (error) {
       console.log(error);
     });
 }
+
+
+
