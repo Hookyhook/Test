b311c677b292d5dada810881bcac5fc564d13a04
Hookyhook <108137850+Hookyhook@users.noreply.github.com>
2023-01-14 11:48:37 +0100

    Finished map production ready

--- OpenLayers/app.js
+++ OpenLayers/app.js
@@ -1,26 +1,5 @@
-// create Stamen layer
-var stamenLayer = new ol.layer.Tile({
-  source: new ol.source.Stamen({
-    layer: "toner",
-  }),
-});
-
-// create a map
-  var map = new ol.Map({
-    target: "map",
-    layers: [stamenLayer],
-    view: new ol.View({
-      center: ol.proj.fromLonLat([0, 0]),
-      zoom: 1,
-    }),
-    controls: [],
-    interactions: [
-    ],
-  });
-
-
-// code for adding marker when an address is typed in
-
+//Define global vars
+var map;
 var markerFeature;
 var markerVectorLayer;
 var markerStyle = new ol.style.Style({
@@ -32,6 +11,32 @@ var markerStyle = new ol.style.Style({
     angle: Math.PI / 4,
   }),
 });
+SetUpMap();
+
+function SetUpMap() {
+  //Uses the stamen library to set up a new Map Layout source
+  var stamenLayer = new ol.layer.Tile({
+    source: new ol.source.Stamen({
+      layer: "toner",
+    }),
+  });
+
+  //The map is created with the source the origin view with zomm is set all controls and interactions are removed
+    map = new ol.Map({
+      target: "map",
+      layers: [stamenLayer],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([0, 0]),
+        zoom: 1,
+      }),
+      controls: [],
+      interactions: [],
+    });
+}
+
+// code for adding marker when an address is typed in
+
+
 
 function searchAddress() {
   var address = document.getElementById("address").value;
@@ -47,21 +52,7 @@ function searchAddress() {
         var lon = data[0].lon;
         var lat = data[0].lat;
         var coord = ol.proj.fromLonLat([lon, lat]);
-        if (markerVectorLayer) {
-          map.removeLayer(markerVectorLayer);
-        }
-        markerFeature = new ol.Feature({
-          geometry: new ol.geom.Point(coord),
-        });
-        markerFeature.setStyle(markerStyle);
-        var vectorSource = new ol.source.Vector({
-          features: [markerFeature],
-        });
-        markerVectorLayer = new ol.layer.Vector({
-          source: vectorSource,
-        });
-        map.addLayer(markerVectorLayer);
-        map.getView().animate({ center: coord, zoom: 16 });
+        setMarker(coord);
       } else {
         console.log("No results found");
       }
@@ -72,4 +63,39 @@ function searchAddress() {
 }
 
 
+
+
+//Starts a navigation to the set marker
+function startNavigation() {
+  if (markerFeature) {
+    var markerCoordinates = markerFeature.getGeometry().getCoordinates();
+    var lonlat = ol.proj.toLonLat(markerCoordinates);
+    console.log(lonlat);
+    var lon = lonlat[0];
+    var lat = lonlat[1];
+    var url = "https://www.google.com/maps/dir/?api=1&destination=" + lat + "," + lon;
+    window.location.href = url;
+  } else {
+    return null;
+  }
+}
+
+//Sets the marker at coordinated with the predefined design removes other layers
+function setMarker(coord) {
+  if (markerVectorLayer) {
+    map.removeLayer(markerVectorLayer);
+  }
+  markerFeature = new ol.Feature({
+    geometry: new ol.geom.Point(coord),
+  });
+  markerFeature.setStyle(markerStyle);
+  var vectorSource = new ol.source.Vector({
+    features: [markerFeature],
+  });
+  markerVectorLayer = new ol.layer.Vector({
+    source: vectorSource,
+  });
+  map.addLayer(markerVectorLayer);
+  map.getView().animate({ center: coord, zoom: 16 });
+}
 
